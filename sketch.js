c7ebdbaea65a1b425c29f8880d9a34ef7447e5d5
asproulos <57708179+asproulos@users.noreply.github.com>
2024-09-13 15:03:15 +0300

    Changes

--- sketch.js
+++ sketch.js
@@ -1,48 +1,250 @@
+/**
+ * @type {{
+*  boardSize: number,
+*  tiles: Array<Array<Tile>>,
+*  size: number,
+*  offsetWidth: number,
+*  offsetHeight: number,
+*  margin: number,
+*  players: Array<Player>,
+*  availableTiles:Array<Tile>
+* }}
+*/
 const globals = {
-  tileArray: [],
-  size: 10,
-  boardSize: 16,
-  offset: 10,
+   boardSize: 16,
+   tiles: null,
+   size: 0,
+   offsetWidth: 0,
+   offsetHeight: 0,
+   margin: 0.2,
+   players: null,
+   availableTiles: [],
 }
 
-//typical board: 16x16
+const tileClr = {
+   R: 255,
+   G: 255,
+   B: 255,
+   A: 255
+}
+
+let availableTileClr = {
+   R: 100,
+   G: 150,
+   B: 200,
+   A: 255
+}
+
 
 function setup() {
-  // put setup code here
-  createCanvas(windowWidth, windowHeight);
-  globals.size = windowHeight / globals.boardSize
-  globals.offset = calcOffset();
+   // put setup code here
+   createCanvas(windowWidth, window.innerHeight);
+   globals.players = [
+       new Player(0, 0, globals.size), // Red
+       new Player(4, 6, globals.size), // Green
+       new Player(4, 10, globals.size), // Blue
+       new Player(5, 5, globals.size) // Yellow
+   ];
 
-  let x, y;
-  for (let i = 0; i <= globals.boardSize; i++) {
-    x = globals.offset + (i * globals.size);
-    console.log(globals.offset);
-    console.log(x);
-    globals.tileArray.push(new Tile(x, y, globals.size, 255))
-    for (let j = 0; j <= globals.boardSize; j++) {
-      y = j * globals.size;
-      globals.tileArray.push(new Tile(x, y, globals.size, 255));
-    }
-  }
+   globals.players[0].clr = {
+       R: 255,
+       G: 0,
+       B: 0,
+       A: 255
+   };
+   globals.players[1].clr = {
+       R: 0,
+       G: 255,
+       B: 0,
+       A: 255
+   };
+   globals.players[2].clr = {
+       R: 0,
+       G: 0,
+       B: 255,
+       A: 255
+   };
+   globals.players[3].clr = {
+       R: 255,
+       G: 255,
+       B: 0,
+       A: 255
+   };
+
+   globals.tiles = loadMap();
+   globals.size = calculateTileSize();
+
+   for (let i = 0; i < globals.players.length; i++) {
+       let player = globals.players[i];
+       player.moveSpecific(player.i, player.j);
+   }
 }
 
 function draw() {
-  // put drawing code here
-  background(200);
-  for (let i = 0; i < globals.tileArray.length; i++) {
-    let tile = globals.tileArray[i];
+   // put drawing code here
+   background(200);
+   let hoverTile = getCurrentTile();
 
-    tile.isInside(mouseX, mouseY);
-    tile.draw(globals.size, globals.offset);
-  }
+   drawTiles(globals.tiles, tileClr);
+
+   drawTiles(globals.availableTiles, availableTileClr);
+
+   drawTiles(globals.players);
+
+   drawTilesBorders(globals.tiles);
+}
+
+/**
+* @param {Array<Array<Tile>> | Array<Tile>} array 
+*/
+function drawTiles(array, clr) {
+   if (array[0] instanceof Array) {
+       array.forEach((innerArray) => drawTiles(innerArray, clr))
+   } else {
+       array.forEach(tile => {
+           clr ? tile.clr = clr : null;
+           tile.draw();
+       });
+   }
+}
+
+function drawTilesBorders(array) {
+   if (array[0] instanceof Array) {
+       array.forEach((innerArray) => drawTilesBorders(innerArray));
+   } else {
+       array.forEach(tile => tile.drawBorders());
+   }
+}
+
+
+/**
+* @param {KeyboardEvent} event 
+*/
+function keyPressed(event) {
+   event.preventDefault();
+}
+
+/**
+* @param {PointerEvent} event 
+*/
+function mouseClicked(event) {
+   let hoverTile = getCurrentTile();
+   if (hoverTile) {
+       let selectedPlayer = null;
+       for (let i = 0; i < globals.players.length; i++) {
+           let player = globals.players[i];
+           if (player.i === hoverTile.i && player.j === hoverTile.j) {
+               selectedPlayer = player;
+           } else {
+               player.isSelected = false;
+           }
+       }
+       if (selectedPlayer) {
+           selectedPlayer.isSelected = true;
+           availableTileClr.R = selectedPlayer.clr.R;
+           availableTileClr.G = selectedPlayer.clr.G;
+           availableTileClr.B = selectedPlayer.clr.B;
+           availableTileClr.A = 100;
+
+           globals.availableTiles = selectedPlayer.getAvailableTiles(globals.tiles);
+       } else {
+           globals.availableTiles = [];
+       }
+   }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-  globals.size = windowHeight / globals.boardSize
-  globals.offset = calcOffset()
+   resizeCanvas(windowWidth, window.innerHeight);
+   globals.size = calculateTileSize();
 }
 
-function calcOffset() {
-  return (windowWidth / 2) - (globals.boardSize * globals.size)
+/**
+* @returns {Array<Array<Tile>>}
+*/
+function loadMap() {
+   /** @type {Array<Array<Tile>>} */
+   let temp = []
+   const cols = globals.boardSize;
+   const rows = globals.boardSize;
+   for (let i = 0; i < cols; i++) {
+       temp.push([]);
+       for (let j = 0; j < rows; j++) {
+           let tile = new Tile(i, j, 1);
+           if (i === 0) {
+               tile.sides.left = true;
+           }
+           if (i === cols - 1) {
+               tile.sides.right = true;
+           }
+           if (j === 0) {
+               tile.sides.top = true;
+           }
+           if (j === rows - 1) {
+               tile.sides.bottom = true;
+           }
+           temp[i].push(tile);
+       }
+   }
+   temp[7][7].sides.top = true;
+   temp[7][7].sides.left = true;
+
+   temp[7][8].sides.bottom = true;
+   temp[7][8].sides.left = true;
+
+   temp[8][7].sides.top = true;
+   temp[8][7].sides.right = true;
+
+   temp[8][8].sides.bottom = true;
+   temp[8][8].sides.right = true;
+
+
+   // custom tiles
+   temp[9][6].sides.right = true;
+   temp[10][5].sides.left = true;
+
+
+   return temp;
+}
+
+/**
+* @returns {number}
+*/
+function calculateTileSize() {
+   let smallSide = Math.min(windowWidth, windowHeight);
+   let length = globals.tiles.length;
+   let size = smallSide / (length + globals.margin);
+
+   globals.offsetWidth = (windowWidth / 2) - ((length * size) / 2);
+   globals.offsetHeight = (windowHeight / 2) - ((length * size) / 2);
+
+   for (let i = 0; i < globals.tiles.length; i++) {
+       for (let j = 0; j < globals.tiles[i].length; j++) {
+           let tile = globals.tiles[i][j];
+
+           tile.i = i;
+           tile.j = j;
+           tile.size = size;
+       }
+   }
+
+   for (let i = 0; i < globals.players.length; i++) {
+       let player = globals.players[i];
+       player.size = size;
+   }
+
+   return size;
+}
+
+/**
+* @returns {Tile | null}
+*/
+function getCurrentTile() {
+   let hoverTile = null;
+   let k = Math.floor((mouseX - globals.offsetWidth) / globals.size);
+   let row = globals.tiles[k];
+   if (row) {
+       let l = Math.floor((mouseY - globals.offsetHeight) / globals.size);
+       hoverTile = row[l];
+   }
+   return hoverTile;
 }
--- tile.js
+++ tile.js
@@ -1,44 +1,253 @@
+
+const Direction = {
+    Top: "top",
+    Right: "right",
+    Bottom: "bottom",
+    Left: "left"
+}
+
 class Tile {
-  constructor(x, y, size, clr) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.clr = clr;
-    this.inside = false;
+    /** @type {{top: boolean, right: boolean, bottom: boolean, left: boolean}}} */
+    sides = {};
+    hasPlayer = false;
+    /**
+     * 
+     * @param {number} i 
+     * @param {number} j 
+     * @param {number} size 
+     * @param {{
+     *  R:number,
+     *  G:number,
+     *  B:number,
+     *  A:number
+     * }} clr 
+     */
+    constructor(i, j, size) {
+        this.i = i;
+        this.j = j;
+        this.size = size;
+        this.clr = {
+            R: 255,
+            G: 255,
+            B: 255,
+            A: 255
+        };
 
-    this.top = false;
-    this.right = false;
-    this.bottom = false;
-    this.left = false;
-  }
+        this.sides[Direction.Top] = false;
+        this.sides[Direction.Right] = false;
+        this.sides[Direction.Bottom] = false;
+        this.sides[Direction.Left] = false;
 
-  draw(size, offset) {
-    if (this.inside)
-      fill(255, 0, 0);
-    else
-      fill(this.clr);
-      
-    this.x = (this.x / this.size) * size;
-    
-    // this moves the entire board :thinking_emoji
-    // this.x = offset + ((this.x / this.size) * size);
-    
-    this.y = (this.y / this.size) * size;
-    this.size = size;
-    rect(this.x, this.y, this.size, this.size);
-  }
+    }
 
-  isInside(mouseX, mouseY) {
-    let minX = this.x;
-    let minY = this.y;
-    let maxX = this.x + this.size;
-    let maxY = this.y + this.size;
-    
-    let minCondition = mouseX > minX && mouseY > minY;
-    let maxCondition = mouseX < maxX && mouseY < maxY;
-    if (minCondition && maxCondition)
-      this.inside = true;
-    else
-      this.inside = false;
-  }
+    get x() {
+        return globals.offsetWidth + (this.i * this.size);
+    }
+
+    get y() {
+        return globals.offsetHeight + (this.j * this.size);
+    }
+
+    get topLeft() {
+        return {
+            x: this.x,
+            y: this.y
+        }
+    }
+
+    get topRight() {
+        return {
+            x: this.x + this.size,
+            y: this.y
+        }
+    }
+
+    get bottomLeft() {
+        return {
+            x: this.x,
+            y: this.y + this.size
+        }
+    }
+
+    get bottomRight() {
+        return {
+            x: this.x + this.size,
+            y: this.y + this.size
+        }
+    }
+
+    draw() {
+        strokeWeight(0.2);
+        fill(this.clr.R, this.clr.G, this.clr.B, this.clr.A);
+        rect(this.x, this.y, this.size, this.size);
+    }
+
+    drawBorders() {
+        if (this.sides.top) {
+            this.#customLine(this.topLeft, this.topRight);
+        }
+        if (this.sides.right) {
+            this.#customLine(this.topRight, this.bottomRight);
+        }
+        if (this.sides.bottom) {
+            this.#customLine(this.bottomLeft, this.bottomRight);
+        }
+        if (this.sides.left) {
+            this.#customLine(this.bottomLeft, this.topLeft);
+        }
+    }
+
+    #customLine(point1, point2) {
+        strokeWeight(3);
+        line(point1.x, point1.y, point2.x, point2.y);
+    }
+
+
+    isInside(mouseX, mouseY) {
+        let minX = this.x;
+        let minY = this.y;
+        let maxX = this.x + this.size;
+        let maxY = this.y + this.size;
+
+        let minCondition = mouseX > minX && mouseY > minY;
+        let maxCondition = mouseX < maxX && mouseY < maxY;
+
+        return minCondition && maxCondition;
+    }
+
+    canExit(moveDirection) {
+        if (moveDirection === Direction.Top) {
+            return !this.sides[Direction.Top]
+        } else if (moveDirection === Direction.Right) {
+            return !this.sides[Direction.Right]
+        } else if (moveDirection === Direction.Bottom) {
+            return !this.sides[Direction.Bottom]
+        } else if (moveDirection === Direction.Left) {
+            return !this.sides[Direction.Left]
+        }
+
+        return false;
+    }
+
+    canEnter(moveDirection) {
+        if (this.hasPlayer) return false;
+
+        if (moveDirection === Direction.Top) {
+            return !this.sides[Direction.Bottom]
+        } else if (moveDirection === Direction.Right) {
+            return !this.sides[Direction.Left]
+        } else if (moveDirection === Direction.Bottom) {
+            return !this.sides[Direction.Top]
+        } else if (moveDirection === Direction.Left) {
+            return !this.sides[Direction.Right]
+        }
+
+        return false;
+    }
+}
+
+class Player extends Tile {
+    #scaleSize = 0.85;
+
+    constructor(i, j) {
+        super(i, j, globals.size);
+
+        this.clr = {
+            R: 150,
+            G: 150,
+            B: 150,
+            A: 255
+        };
+
+        this.width = globals.size * this.#scaleSize;
+        this.isSelected = false;
+    }
+
+    draw() {
+        this.width = globals.size * this.#scaleSize;
+        fill(this.clr.R, this.clr.G, this.clr.B, this.clr.A);
+        strokeWeight(1)
+        ellipse(this.x + this.size / 2, this.y + this.size / 2, this.width);
+    }
+
+    /**
+     * @param {Array<Array<Tile>>} board
+     * @returns {Array<Tile>}
+     */
+    getAvailableTiles(board) {
+        let temp = [];
+
+        //top
+        let prevTile = null;
+        for (let j = this.j; j >= 0; j--) {
+            const tile = board[this.i][j];
+            if (!prevTile) {
+                prevTile = tile;
+                continue;
+            }
+            if (!prevTile.canExit(Direction.Top) || !tile.canEnter(Direction.Top)) {
+                break;
+            }
+            temp.push(tile);
+            prevTile = tile;
+        }
+
+        //right
+        prevTile = null;
+        for (let i = this.i; i < board.length; i++) {
+            const tile = board[i][this.j];
+            if (!prevTile) {
+                prevTile = tile;
+                continue;
+            }
+            if (!prevTile.canExit(Direction.Right) || !tile.canEnter(Direction.Right)) {
+                break;
+            }
+            temp.push(tile);
+            prevTile = tile;
+        }
+
+        //bottom
+        prevTile = null;
+        for (let j = this.j; j < board.length; j++) {
+            const tile = board[this.i][j];
+            if (!prevTile) {
+                prevTile = tile;
+                continue;
+            }
+            if (!prevTile.canExit(Direction.Bottom) || !tile.canEnter(Direction.Bottom)) {
+                break;
+            }
+            temp.push(tile);
+            prevTile = tile;
+        }
+
+        //left
+        prevTile = null;
+        for (let i = this.i; i >= 0; i--) {
+            const tile = board[i][this.j];
+            if (!prevTile) {
+                prevTile = tile;
+                continue;
+            }
+            if (!prevTile.canExit(Direction.Left) || !tile.canEnter(Direction.Left)) {
+                break;
+            }
+            temp.push(tile);
+            prevTile = tile;
+        }
+
+        return temp;
+    }
+
+    move() {
+
+    }
+
+    moveSpecific(i, j) {
+        globals.tiles[this.i][this.j].hasPlayer = false;
+        this.i = i;
+        this.j = j;
+        globals.tiles[this.i][this.j].hasPlayer = true;
+    }
 }
